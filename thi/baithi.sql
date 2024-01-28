51ea6c77244d5cc7db59709e4885985638af93bd
hoang07042005 <doviethoang07042005@gmail.com>
2024-01-28 12:51:41 +0700

    thi

---
+++ thi/baithi.sql
@@ -0,0 +1,9 @@
+CREATE TABLE baithi(
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          name VARCHAR(250),
+                          email VARCHAR(250)
+);
+
+INSERT INTO baithi (name, email) VALUES
+                                       ('hoang', 'doviethoang07042005@gmail.com'),
+                                       ('son', 'theson123@ggmail.com');.
