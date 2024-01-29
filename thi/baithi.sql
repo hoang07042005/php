4ebdd04dd23d8c55caf343ce26c6625ca7218dff
hoang07042005 <142959468+hoang07042005@users.noreply.github.com>
2024-01-29 10:39:24 +0700

    Delete thi directory

--- thi/baithi.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contacts_table(
-                          id INT PRIMARY KEY AUTO_INCREMENT,
-                          name VARCHAR(250),
-                          phone_number VARCHAR(250)
-);
-
-INSERT INTO contacts_table (name, phone_number) VALUES
-                                       ('hoang', '0352335720');
